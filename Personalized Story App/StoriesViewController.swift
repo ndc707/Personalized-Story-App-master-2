f3255e7214699d4c6cc96055d99182ac974038ef
Student <student@bluc02mf2v9fh51.local>
2019-04-16 11:40:31 -0500

    Initial Commit

---
+++ Personalized Story App/StoriesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Personalized Story App
+//
+//  Created by Emily Kirk on 4/8/19.
+//  Copyright © 2019 Emily Kirk. All rights reserved.
+//
+
+import UIKit
+
+class StoriesViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
